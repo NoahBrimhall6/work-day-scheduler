d882414d4e0d665c36cd2f7ffadc938c3849b472
Noah Brimhall <noahbrimhall6@gmail.com>
2022-08-23 22:52:39 -0600

    Added for loop to build time blocks in script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 $("#currentDay").text(moment().format("dddd, MMMM do"));
+
+for (var i = 0; i < 9; i++) {
+    var row = $("<div>");
+    row.addClass("row");
+
+    var hour = $("<div>");
+    var timeBlock = $("<div>");
+    var saveBtn = $("<div>");
+
+    hour.addClass("hour");
+    timeBlock.addClass("time-block");
+    saveBtn.addClass("saveBtn");
+
+    $(".container").append(row);
+    row.append(hour);
+    row.append(timeBlock);
+    row.append(saveBtn);
+}
