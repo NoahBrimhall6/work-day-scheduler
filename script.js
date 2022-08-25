cb96d1717c30240a48db0561aa5f9726dde1e215
Noah Brimhall <noahbrimhall6@gmail.com>
2022-08-24 20:41:31 -0600

    allowed inputs to be saved in local storage

--- script.js
+++ script.js
@@ -1,41 +1,74 @@
-$("#currentDay").text(moment().format("dddd, MMMM do"));
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 var currentHour = moment().format("H");
 
-for (var i = 9; i < 18; i++) {
-    var row = $("<div>");
-    row.addClass("row");
+var container = $(".container");
 
-    var hour = $("<div>");
-    var timeBlock = $("<div>");
-    var saveBtn = $("<button>");
 
-    hour.addClass("hour");
-    hour.addClass("col-1");
-    timeBlock.addClass("time-block");
-    timeBlock.addClass("col-10");
-    saveBtn.addClass("saveBtn");
-    saveBtn.addClass("col-1");
-    saveBtn.addClass("text-center");
 
-    if (i < currentHour) {
-        timeBlock.addClass("past");
-    } else if (i === currentHour) {
-        timeBlock.addClass("present");
-    } else {
-        timeBlock.addClass("future");
+var inputs = ["","","","","","","","",""];
+
+function renderTimeBlocks() {
+    container.innerHTML = "";
+
+    for (var i = 9; i < 18; i++) {
+        var row = $("<div>");
+        row.addClass("row");
+
+        var hour = $("<div>");
+        var timeBlock = $("<textarea>");
+        var saveBtn = $("<button>");
+
+        hour.addClass("hour");
+        hour.addClass("col-1");
+        timeBlock.addClass("col-10");
+        timeBlock.attr("contentEditable", "true")
+        saveBtn.addClass("saveBtn");
+        saveBtn.addClass("col-1");
+        saveBtn.attr("data-index", i - 9);
+    
+        if (i < currentHour) {
+            timeBlock.addClass("past");
+        } else if (i == currentHour) {
+            timeBlock.addClass("present");
+        } else {
+            timeBlock.addClass("future");
+        }
+    
+        if (i < 12) {
+            hour.text(i + "AM");
+        } else if (i === 12) {
+            hour.text("12PM")
+        } else {
+            hour.text(i - 12 + "PM");
+        }
+        saveBtn.text("save");
+
+        timeBlock.text(inputs[i - 9]);
+    
+        container.append(row);
+        row.append(hour);
+        row.append(timeBlock);
+        row.append(saveBtn);
     }
-
-    if (i < 12) {
-        hour.text(i + "AM");
-    } else if (i === 12) {
-        hour.text("12PM")
-    } else {
-        hour.text(i - 12 + "PM");
-    }
-    saveBtn.text("save");
-
-    $(".container").append(row);
-    row.append(hour);
-    row.append(timeBlock);
-    row.append(saveBtn);
 }
+
+function storeInput(event) {
+    var userInput = $(event.target).siblings("textarea").val();
+    var index = $(event.target).attr("data-index");
+    inputs.splice(index, 1, userInput);
+    localStorage.setItem("inputs", JSON.stringify(inputs));
+}
+
+function init() {
+    var storedInputs = JSON.parse(localStorage.getItem("inputs"));
+    if (storedInputs !== null) {
+        for (var i = 0; i < 9; i++) {
+            inputs.splice(i, 1, storedInputs[i]);
+        }
+    }
+    renderTimeBlocks();
+}
+
+$(".container").on("click", ".saveBtn", storeInput);
+
+init();
